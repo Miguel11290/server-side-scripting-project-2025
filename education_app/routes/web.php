438f598cbb6d94853facd8b2aaf4f215819ad166
Miguel11290 <miguel.bonello.e21277@mcast.edu.mt>
2025-03-09 11:11:01 +0000

    Created Student and College routes

--- education_app/routes/web.php
+++ education_app/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//College routes
+Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index'); //display all colleges
+Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create'); //display form to create a new college
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit'); //display form to edit a college
+
+//Student routes
+Route::get('/students', [StudentController::class, 'index'])->name('students.index'); //display all students
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create'); //display form to create a new student
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit'); //display form to edit a student
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy'); //delete a student
+
